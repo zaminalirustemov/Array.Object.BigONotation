e6919e8091c98fb0dc8b6fcfc5b06f402c3dd677
zaminalirustemov <2g17rck@code.edu.az>
2022-11-02 02:10:55 +0400

    Homework

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            int cem = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i]> 99)
+                {
+                    cem += arr[i];
+                }
+            }
+            Console.Write($"Ededlerinin cemi:{cem} ");
+        }
+    }
+}
---
+++ task2/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] arr = { 3, 1, 5, 2, 5, 1, 3, 1, 8, 7, 1 };
+            int[] count = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                count[arr[i]] ++;
+
+      
+            }
+            for(int i = 0; i < count.Length; i++)
+                Console.WriteLine("{0}="+ count[i] , i);
+            
+        }
+    }
+}
